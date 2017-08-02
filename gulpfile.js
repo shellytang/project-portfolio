b320ec4d5ebe79c32a8e2bf02c5ad0e451265d05
Shelly Tang <shellytang@gmail.com>
2017-08-02 11:56:10 -0700

    restructured directories

--- gulpfile.js
+++ gulpfile.js
@@ -2,43 +2,29 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
-// const rename = require('gulp-rename');
-// const uglify = require('gulp-uglify');
 const browserSync = require('browser-sync');
 const autoprefixer = require('gulp-autoprefixer');
 
 // launch server
-gulp.task('browser-sync', ['styles', 'scripts'], function() {
+gulp.task('browser-sync', ['styles'], function() {
   browserSync.init({
-    server: './',
+    server: './public',
     port: 3000,
   });
 });
 
 // compile files from SCSS
 gulp.task('styles', function () {
-  return gulp.src('./scss/main.scss')
+  return gulp.src('./public/scss/main.scss')
   .pipe(sass({
     includePaths: ['scss'],
     onError: browserSync.notify
   }))
   .pipe(autoprefixer(['last 2 versions'], { cascade: true }))
   .pipe(cleanCSS())
-  .pipe(gulp.dest('./css/'))
+  .pipe(gulp.dest('./public/css/'))
   .pipe(browserSync.reload({stream:true}))
-});
-
-//script paths - might not be necessary
-gulp.task('scripts', function() {
-  return gulp.src('./js/**/*.js')
-      .pipe(concat('main.js'))
-      .pipe(gulp.dest('./'))
-      // .pipe(rename('main.min.js'))
-      // .pipe(uglify())
-      // .pipe(gulp.dest('./'))
-      .pipe(browserSync.reload({stream:true}));
 });
 
 //reload when html changes
@@ -46,9 +32,8 @@ gulp.task('html', function() {
   browserSync.reload();
 });
 
-gulp.task('watch', ['scripts', 'styles', 'html'], function() {
-  gulp.watch('./js/**/*.js', ['scripts']);
-  gulp.watch('./scss/main.scss', ['styles']);
+gulp.task('watch', ['styles', 'html'], function() {
+  gulp.watch('./public/scss/main.scss', ['styles']);
   gulp.watch(['index.html'], ['html']);
 });
 
